7db0788157f6d433da45be0315bcb2bfdb83f653
Samoylov Zakhar <samoilov.zahar20121@gmail.com>
2021-09-22 02:30:28 +0300

    link server change

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,4 @@
 import { Desk } from './components/Desk';
 
-const desk = new Desk('.desk', 'http://localhost:7070');
+const desk = new Desk('.desk', 'https://hw-help-desk.herokuapp.com');
 window.desk = desk;
-
-// (async () => {
-//   const post = await fetch(
-//     'http://localhost:7070?method=createTicket&name=qwe&description=asdasd',
-//     {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//     }
-//   );
-
-//   post.json().then((response) => console.log(response));
-// })();
